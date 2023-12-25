3b8cb2975501a008df905c0444a5a4859ba3ccda
Mar0 <106659477+MarO-cmd@users.noreply.github.com>
2023-12-25 13:54:16 +0200

    Update Program.cs

--- Producer and consumer/Producer and consumer/Program.cs
+++ Producer and consumer/Producer and consumer/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,131 +11,68 @@ namespace Producer_and_consumer
     {
 
         static Queue<int> buffer = new Queue<int>();
-        static int bufferSize = 5;
-        static int itemCount = 10;
-        static void Producer()  // which produce 
+        static int size = 5;
+
+        static int item = 10;
+
+        static void producer()
         {
-            for (int i = 0; i < itemCount; i++)
+            for (int i = 0; i < item; i++)
             {
                 lock (buffer)
                 {
-                    while (buffer.Count >= bufferSize)
+                    if (buffer.Count >= size)
                     {
-                        Console.WriteLine("Buffer is full. Producer is waiting...");
+                        Console.WriteLine("the buffer is fuLL");
                         Monitor.Wait(buffer);
                     }
+
                     buffer.Enqueue(i);
-                    Console.WriteLine("Produced: " + i);
-                    // Notify the consumer that an item is available
+                    Console.WriteLine($"produced :{i} ");
                     Monitor.Pulse(buffer);
                 }
-                // Simulate some work done by the producer
-                Thread.Sleep(100);
             }
+
+            Thread.Sleep(200);
         }
-        static void Consumer() // which consume
+        static void consumer()
         {
-            for (int i = 0; i < itemCount; i++)
+            for (int i = 0; i < item; i++)
             {
                 lock (buffer)
                 {
-                    while (buffer.Count == 0)
+                    if (buffer.Count == 0)
                     {
-                        Console.WriteLine("Buffer is empty. Consumer is waiting...");
+                        Console.WriteLine("the buffer is empty");
                         Monitor.Wait(buffer);
                     }
-                    int consumedItem = buffer.Dequeue();
-                    Console.WriteLine("Consumed: " + consumedItem);
-                    // Notify the producer that space is available in the buffer
+
+                    int val = buffer.Dequeue();
+                    Console.WriteLine($"consumed :{val} ");
                     Monitor.Pulse(buffer);
                 }
-                // Simulate some work done by the consumer
-                Thread.Sleep(200);
-                
             }
+
+            Thread.Sleep(500);
         }
 
-        
+
+
         static void Main(string[] args)
         {
-            var producerThread = new Thread(Producer);
-            var consumerThread = new Thread(Consumer);
 
 
 
-            producerThread.Start();
-            consumerThread.Start();
-            producerThread.Join();
-            consumerThread.Join();
+            Thread th1 = new Thread(producer);
+            Thread th2 = new Thread(consumer);
 
-            Console.WriteLine("Finished processing.");
+
+            th1.Start();
+            th2.Start();
+            th1.Join();
+            th2.Join();
+
         }
-
-
-
-        //static Queue<int> buffer = new Queue<int>();
-        //static int size = 5;
-
-        //static int item = 10;
-
-        //static void producer()
-        //{
-        //    for (int i = 0; i < item; i++)
-        //    {
-        //        lock (buffer)
-        //        {
-        //            if (buffer.Count >= size)
-        //            {
-        //                Console.WriteLine("the buffer is fuLL");
-        //                Monitor.Wait(buffer);
-        //            }
-
-        //            buffer.Enqueue(i);
-        //            Console.WriteLine($"produced :{i} ");
-        //            Monitor.Pulse(buffer);
-        //        }
-        //    }
-
-        //    Thread.Sleep(100);
-        //}
-        //static void consumer()
-        //{
-        //    for (int i = 0; i < item; i++)
-        //    {
-        //        lock (buffer)
-        //        {
-        //            if (buffer.Count == 0)
-        //            {
-        //                Console.WriteLine("the buffer is empty");
-        //                Monitor.Wait(buffer);
-        //            }
-
-        //            int val = buffer.Dequeue();
-        //            Console.WriteLine($"consumed :{val} ");
-        //            Monitor.Pulse(buffer);
-        //        }
-        //    }
-
-        //    Thread.Sleep(200);
-        //}
-
-
-
-        //static void Main(string[] args)
-        //{
-
-
-
-        //    Thread th1 = new Thread(producer);
-        //    Thread th2 = new Thread(consumer);
-
-
-        //    th1.Start();
-        //    th2.Start();
-        //    th1.Join();
-        //    th2.Join();
-
-        //}
 
 
 
